eb8a888829f1c603b3f26a2d15b5a87551328ddb
Ivan <ivan.grubor@gmail.com>
2021-04-01 15:09:15 +0200

    Sitne ispravke

--- models/Person.js
+++ models/Person.js
@@ -1,5 +1,10 @@
 class Person {
     constructor(ime,prez) {
+
+        if (this.constructor === Person) {
+            throw new Error('This class is defined as abstract.')
+        }
+
         this.ime = ime;
         this.prezime = prez;
     }
